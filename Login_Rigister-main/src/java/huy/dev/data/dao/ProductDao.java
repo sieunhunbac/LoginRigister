769c146b5d1ada62d4ade843ef46647b8b564309
sieunhunbac <huy.kha0741@gmail.com>
2024-10-16 23:13:13 +0700

    asd

---
+++ Login_Rigister-main/src/java/huy/dev/data/dao/ProductDao.java
@@ -0,0 +1,13 @@
+package hoa.dev.data.dao;
+
+import java.util.List;
+
+import hoa.dev.data.model.Product;
+
+public interface ProductDao {
+    public boolean insert(Product product);
+    public boolean update(Product product);
+    public boolean delete(int id);
+    public Product find(int id);
+    public List<Product> findAll();
+}
